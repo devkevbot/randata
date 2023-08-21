ad6f906abc5ae15775541d79bea3721a41abf6f6
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-08-20 18:49:43 -0700

    release: v0.3.0
    Add functionality to produce a random IPv4 and IPv6 address

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ fn main() {
             let format = format.unwrap_or_default();
             gen_color_string(&format)
         }
+        Commands::IpAddr { format } => {
+            let format = format.unwrap_or_default();
+            gen_ip_addr(&format)
+        }
     };
 
     println!("{output}");
@@ -65,6 +69,12 @@ enum Commands {
         #[arg(short, long)]
         format: Option<ColorFormat>,
     },
+    // Generates a random IP address.
+    #[command(name = "ip")]
+    IpAddr {
+        #[arg(short, long)]
+        format: Option<IpAddrFormat>,
+    },
 }
 
 #[derive(Debug, Clone, ValueEnum, Default)]
@@ -78,9 +88,9 @@ enum ColorFormat {
 fn gen_color_string(fmt: &ColorFormat) -> String {
     let mut rng = rand::thread_rng();
 
-    let red = rng.gen_range(0..=255);
-    let green = rng.gen_range(0..=255);
-    let blue = rng.gen_range(0..=255);
+    let red = rng.gen_range(u8::MIN..=u8::MAX);
+    let green = rng.gen_range(u8::MIN..=u8::MAX);
+    let blue = rng.gen_range(u8::MIN..=u8::MAX);
 
     let hue = rng.gen_range(0..=360);
     let saturation = rng.gen_range(0..=100);
@@ -90,5 +100,41 @@ fn gen_color_string(fmt: &ColorFormat) -> String {
         ColorFormat::Hex => format!("#{red:02x}{green:02x}{blue:02x}"),
         ColorFormat::Hsl => format!("({hue},{saturation}%,{lightness}%)"),
         ColorFormat::Rgb => format!("({red},{green},{blue})"),
+    }
+}
+
+#[derive(Debug, Clone, ValueEnum, Default)]
+enum IpAddrFormat {
+    #[default]
+    Ipv4,
+    Ipv6,
+}
+
+fn gen_ip_addr(fmt: &IpAddrFormat) -> String {
+    let mut rng = rand::thread_rng();
+
+    match fmt {
+        IpAddrFormat::Ipv4 => {
+            let num_groups = 4;
+            let mut groups = Vec::with_capacity(num_groups);
+
+            for _ in 0..num_groups {
+                let group = rng.gen_range(u8::MIN..=u8::MAX);
+                groups.push(group.to_string())
+            }
+
+            groups.join(".")
+        }
+        IpAddrFormat::Ipv6 => {
+            let num_groups = 8;
+            let mut groups = Vec::with_capacity(num_groups);
+
+            for _ in 0..num_groups {
+                let group = rng.gen_range(u16::MIN..=u16::MAX);
+                groups.push(format!("{group:04x}"))
+            }
+
+            groups.join(":")
+        }
     }
 }
